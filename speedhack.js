fe9b3abd487550a4ba0fcdc99b3fe6c37e6ce824
Maxence Frenette <maxence.frenette@gmail.com>
2022-10-02 14:23:00 -0700

    Add speedhack code

---
+++ speedhack.js
@@ -0,0 +1,27 @@
+// Speed up time by this factor
+const speedup = 1000;
+
+// Save initial timestamp
+const initialTimestamp = Date.now();
+
+// Monkey-patch Date.now()
+const oldDateNow = Date.now;
+window.Date.now = () => {
+    return initialTimestamp + speedup * (oldDateNow.call(Date) - initialTimestamp);
+}
+
+// Monkey-patch new Date()
+const OldDate = Date;
+class SpeedupDate extends OldDate {
+    constructor(...args) {
+        if (args.length === 0) {
+            super(Date.now());
+        } else {
+            super(...args);
+        }
+    }
+}
+window.Date = SpeedupDate;
+
+// Monkey-patch requestAnimationFrame
+// TODO
